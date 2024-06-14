8d980c87f203dc2ff5ffd17a6ba5f33b7013b3c3
Anaxar2 <aaran.barber@gmail.com>
2024-06-14 12:52:46 +0100

    Grey Box
    Capsules for Player and NPC

---
+++ Week 12 Challenge/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //horizontalInput = Input.GetAxis
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
